3fa5865e6237452480562be027a0cd8ce97938b1
José Felipe <20201064010007@cang406739.ifrn.local>
2023-11-29 10:23:18 -0300

    Validação 01

--- app/models/Bibliotecarios.java
+++ app/models/Bibliotecarios.java
@@ -3,6 +3,12 @@ package models;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import play.data.validation.MaxSize;
+import play.data.validation.MinSize;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 import play.libs.Crypto;
 
@@ -11,10 +17,23 @@ import play.libs.Crypto;
 @Entity
 public class Bibliotecarios extends Model {
 	
+	@Required
+	@Min(3)
+	@Max(50)
 	public String nome;
+	
+	@Required
+	@MinSize(11)
+	@MaxSize(11)
 	public long cpf;
+	
+	@Required
 	public String email;
+	
+	@Required
 	public String senha;
+	
+	@Required
 	public String funcao;
 
 	
